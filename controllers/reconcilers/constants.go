fea0a1226b03db3fc1481c7a6cd8d18a76eeaf9c
Olga Lavtar <olavtar@redhat.com>
2022-08-09 16:11:37 -0700

    Changed CRUNCHY_BRIDGE_CSV to match the catalog img

--- controllers/reconcilers/constants.go
+++ controllers/reconcilers/constants.go
@@ -13,7 +13,7 @@ const (
 
 	// CRUNCHY_BRIDGE
 	CRUNCHY_BRIDGE_CATALOG_IMG = "quay.io/olavtar/crunchy-bridge-operator:v0.0.5-dev"
-	CRUNCHY_BRIDGE_CSV         = "crunchy-bridge-operator.v0.0.5"
+	CRUNCHY_BRIDGE_CSV         = "crunchy-bridge-operator.v0.0.5-dev"
 	CRUNCHY_BRIDGE_NAME        = "crunchy-bridge"
 	CRUNCHY_BRIDGE_DISPLAYNAME = "Crunchy Bridge Operator"
 	CRUNCHY_BRIDGE_DEPLOYMENT  = "crunchy-bridge-operator-controller-manager"
